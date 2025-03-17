423e53be647e45b94cd298372a5c646dd6d4a71f
amanglani2503 <ambermanglani8871@gmail.com>
2025-03-17 15:05:41 +0530

    [Amber] Add. Add User entity, Role enum, and User repository

---
+++ src/main/java/com/example/user_service/model/User.java
@@ -0,0 +1,34 @@
+package com.example.user_service.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
