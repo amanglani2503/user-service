f5e77491c49ed9111472635357f2f8fbde06c5ee
amanglani2503 <ambermanglani8871@gmail.com>
2025-03-18 15:34:53 +0530

    [Amber] Refactor. Return JWT in JSON response

--- src/main/java/com/example/user_service/controller/AuthController.java
+++ src/main/java/com/example/user_service/controller/AuthController.java
@@ -1,24 +1,37 @@
 package com.example.user_service.controller;
 
+import com.example.user_service.model.Role;
 import com.example.user_service.model.User;
 import com.example.user_service.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
     private AuthService authService;
 
     @PostMapping("/register")
     public User register(@RequestBody User user){
+        user.setRole(Role.valueOf("PASSENGER"));
         return authService.register(user);
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody User user){
-        return authService.login(user);
+    public ResponseEntity<Map<String, String>> login(@RequestBody User user){
+        String token = authService.login(user);
+
+        if (token == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid credentials"));
+        }
+        return ResponseEntity.ok(Map.of("token", token));
     }
 }
